625b5f88457dc4972cc3c0d4457f42674ccfad26
Diego200509 <diegojijon2005@gmail.com>
2025-09-13 22:17:02 -0500

    Se creo la seccion de historial y sus diseños

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,5 @@ Route::view('/reset-password', 'app');
 Route::view('/goals', 'app'); 
 Route::view('/dashboard', 'app');
 Route::view('/profile', 'app');
+Route::view('/history', 'app'); // Nueva ruta para el historial
 
