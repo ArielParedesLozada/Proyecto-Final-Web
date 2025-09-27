08127e4307bc8ee9b8e8333c22ec4b1849510627
Diego200509 <diegojijon2005@gmail.com>
2025-09-27 03:39:36 -0500

    Se corrigio el borrado de metas que se borre la trasacciones de esa meta

--- app/Models/Goal.php
+++ app/Models/Goal.php
@@ -28,4 +28,22 @@ class Goal extends Model
     {
         return $this->hasMany(Transaction::class);
     }
+
+    protected static function booted()
+    {
+        static::deleting(function (Goal $goal) {
+            if ($goal->isForceDeleting()) {
+                // hard delete de la meta => hard delete de transacciones
+                $goal->transactions()->withTrashed()->forceDelete();
+            } else {
+                // soft delete de la meta => soft delete de transacciones
+                $goal->transactions()->delete();
+            }
+        });
+
+        static::restoring(function (Goal $goal) {
+            // restaurar transacciones si restauras la meta
+            $goal->transactions()->withTrashed()->restore();
+        });
+    }
 }
